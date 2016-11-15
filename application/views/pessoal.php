b4bfe20d4c668ba1c6a2e309f2ae049f3bff4465
victor hugo Valle <vhavhugo@yahoo.com.br>
2016-11-15 06:20:49 +0000

    Form do formulario da página de contato, arrumando páginas, inserindo base_url

---
+++ application/views/pessoal.php
@@ -0,0 +1,19 @@
+<section class="module">
+    <div class="container-fluid container-custom">
+        <div class="row">
+            <div class="col-sm-4">
+              <h6>Personal Info.</h6>
+            </div>
+            <div class="col-sm-8">
+                <ul>
+                    <li>Name: Robert Fields</li>
+                    <li>Birthdate: September 28, 1990</li>
+                    <li>Address: 122 King St, Melbourne VIC</li>
+                    <li>Phone: +44 1234 56 78</li>
+                    <li>E-mail: <a href="#">info@yourdomain.com</a></li>
+                    <li>Website: <a href="#">mysite.com</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
